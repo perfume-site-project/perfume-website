2c524bd072db04afabffc3e847686efec9d9c75b
Github <choeun123456@daum.net>
2022-05-19 10:14:42 +0900

    frame

--- perfproject/src/App.js
+++ perfproject/src/App.js
@@ -1,12 +1,27 @@
-import './App.css';
 import Id from './component/Id'
+import Password from './component/Password'
+import Passwordcheck from './component/Passwordcheck';
+import Name from './component/Name';
+import Contact from './component/Contact';
+import Birthdate from './component/Birthdate';
+import Terms from './component/Terms';
+import Join from './component/Join';
+import './App.css';
 
 function App() {
-  return (
+    return (
     <div className="App">
-        <Id></Id>
+        <Id/>
+        <Password/>
+        <Passwordcheck/>
+        <Name/>
+        <Contact/>
+        <Birthdate/>
+        <Terms/>
+        <Join/>
     </div>
   );
 }
+
 
 export default App;
---
+++ perfproject/src/component/Name.js
@@ -0,0 +1,13 @@
+import './Name.css'
+
+export default function Name() {
+    return (
+    <div className='name-wrapper'>
+        <div>
+            <p>이름</p>
+        </div>
+        <input/>
+    </div>
+    );
+}
+
