b9ff7d8befde1713acaf83e7b6a302b9d77a1d37
subin1509 <subin1509@naver.com>
2022-05-20 22:15:33 +0900

    메인페이지 향수 이미지 추가

--- perfume-website/perfume-website/src/Components/Main.js
+++ perfume-website/perfume-website/src/Components/Main.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 import styles from '../assets/css/Main.module.css'
 import main_logo from "../assets/images/main_logo.png";
+import main_img from "../assets/images/main.gif";
 import PerfumeList from "./PerfumeList";
 
 const Main = () => {
@@ -10,23 +11,24 @@ const Main = () => {
 
     return(
         <div className="Main">
-            <div className={styles.head_container}>
-                <PerfumeList/>
-                <div className={styles.login_box}>
-                    <Link to={"/login"} className={styles.text}>로그인</Link>
-                    <p className={styles.text}>/</p>
-                    <Link to={"/join"} className={styles.text}>회원가입</Link>
+                <div className={styles.head_container}>
+                    <PerfumeList/>
+                    <img className={styles.main_img} src={main_img} alt="main" />
+                    <div className={styles.login_box}>
+                        <Link to={"/login"} className={styles.text}>로그인</Link>
+                        <p className={styles.text}>/</p>
+                        <Link to={"/join"} className={styles.text}>회원가입</Link>
+                    </div>
+                    <div className="logo_box">
+                        <Link to={"/"}>
+                            <img className={styles.main_logo} src={main_logo} alt="logo" />
+                        </Link>
+                    </div>
                 </div>
-                <div className="logo_box">
-                    <Link to={"/"}>
-                        <img src={main_logo} alt="logo" />
-                    </Link>
+                <div className={styles.search_bar}>
+                    <input className={styles.search_input} value={searchWord}
+                        onChange={(e)=>{setSearchWord(e.target.value);}}/>
                 </div>
-            </div>
-            <div className={styles.search_bar}>
-                <input className={styles.search_input} value={searchWord}
-                       onChange={(e)=>{setSearchWord(e.target.value);}}/>
-            </div>
         </div>
     );
 }
