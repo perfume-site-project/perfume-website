34b626b1bc3b83323a86f3b7d1ba69b0f839424b
subin1509 <subin1509@naver.com>
2022-06-30 21:15:39 +0900

    카테고리 선택 시 이미지 변경

--- perfume-website/src/Components/ListElement.js
+++ perfume-website/src/Components/ListElement.js
@@ -8,9 +8,13 @@ const ListElement = (props) => {
     const [loc, setLoc] = useState(props.loc);
     const locNo = 'loc'+ loc;
     const cx = classNames.bind(styles);
+    const sendType = () => {
+        props.getType(props.type);
+    }
+
 
     return (
-        <div className={cx('list_element', `${locNo}`)}> #{props.id} {props.type} </div>
+        <div className={cx('list_element', `${locNo}`)} onMouseOver={ sendType }> #{props.id} {props.type} </div>
     );
 }
 
