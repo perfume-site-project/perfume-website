bbc90169a78a6c862bdd14710c2cffbd61c82ed1
Kang Byeonghyeon <rkdqudgus9806@gmail.com>
2022-07-23 22:28:14 +0900

    Feat: 아임포트 가상 결제시스템 API 추가

---
+++ src/Components/Order/Payment.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+
+const Payment = () => {
+  useEffect(() => {
+    const jquery = document.createElement("script");
+    jquery.src = "https://code.jquery.com/jquery-1.12.4.min.js";
+    const iamport = document.createElement("script");
+    iamport.src = "https://cdn.iamport.kr/js/iamport.payment-1.1.7.js";
+    document.head.appendChild(jquery);
+    document.head.appendChild(iamport);
+    return () => {
+      document.head.removeChild(jquery);
+      document.head.removeChild(iamport);
+    }
+  }, []);
+  return ();
+}
+
+export default Payment;
